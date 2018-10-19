46d78b246e2df413502d916020186ea1c810e664
happy-pig <994399707@qq.com>
2018-10-19 17:39:32 +0800

    push program

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-console": "off",
+        "no-debugger": "off",
+        "no-cond-assign": [
+            "error",
+            "except-parens"
+        ],
+        "curly": [
+            "error",
+            "all"
+        ],
+        "eqeqeq": [
+            "error",
+            "always"
+        ]
+    }
+};
